0cf36fae2da7f4a391ba980b2ea3daf7bfdb06c2
florian <floriankindt@hotmail.com>
2016-10-06 15:13:49 +0200

    basic controller scripts

---
+++ ControllerTest/Assets/Scripts/TestControllerTwo.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestControllerTwo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 inputDirection = Vector3.zero;
+        inputDirection.z = Input.GetAxis("JoystickHorizontal");
+
+        if (inputDirection.z >= 0.1 || inputDirection.z <= -0.1)
+            transform.Translate(0, 0, -inputDirection.z / 2);
+
+        Debug.Log("x: " + inputDirection.x);
+        Debug.Log("y: " + inputDirection.z);
+
+        Vector3 rotateDirection = Vector3.zero;
+        rotateDirection.y = Input.GetAxis("JoystickVertical");
+
+        if(rotateDirection.y >= 0.1 || rotateDirection.y <= -0.1)
+        {
+            transform.Rotate(rotateDirection);
+        }
+    }
+}
---
+++ ControllerTest/Assets/Scripts/TestControlsOne.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestControlsOne : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("JoystickVertical");
+        inputDirection.z = Input.GetAxis("JoystickHorizontal");
+
+        if ((inputDirection.x >= 0.1 || inputDirection.x <= -0.1) || (inputDirection.z >= 0.1 || inputDirection.z <= -0.1))
+            transform.Translate(inputDirection.x / 2, 0, -inputDirection.z  / 2);
+
+        Debug.Log("x: " + inputDirection.x);
+        Debug.Log("y: " + inputDirection.z);
+
+        if(Input.GetButton("Button3"))
+        {
+            transform.Rotate(new Vector3(0, -1, 0));
+        }
+        if (Input.GetButton("Button4"))
+        {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+    }
+}
